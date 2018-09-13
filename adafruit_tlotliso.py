dcfb985117b9f9209763e7e1943340641c3db7c6
Tlotliso <tlotlisomapana@gmail.com>
2018-09-13 02:01:16 +0200

    Initialised SPI on RPi using the imported Adafruit library to work with the MCP3008

---
+++ adafruit_tlotliso.py
@@ -0,0 +1,47 @@
+# !/usr/bin/python
+
+import RPi.GPIO as GPIO
+import Adafruit_MCP3008 as MCP
+import time
+import os
+
+
+# Set GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# define pins
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
+
+# setup SPI pins for I/O
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
+# initialise communication with MCP3008
+mcp = MCP.MCP3008 (clk=SPICLK, cs=SPICS, mosi=SPIMOSI, miso=SPIMISO)
+
+# create a global variable to store adc values
+values = [0]*8
+
+# delay between each reading
+delay = 0.5
+
+try:
+	while True:
+		for i in range(8):
+			values[i] = mcp.read_adc(i)
+
+		# provide delay
+		time.sleep(delay)
+
+		print values
+
+except KeyboardInterrupt:
+	pass
+
+# cleanup GPIO when finished
+GPIO.cleanup()
