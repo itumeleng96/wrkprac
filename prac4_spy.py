024b13e765758678d62b8e0cdc5ffbe06d7024a1
itumeleng96 <itukzz96@gmail.com>
2018-09-15 07:32:23 +0000

    modified structure code

--- prac4_spy.py
+++ prac4_spy.py
@@ -22,25 +22,8 @@ spi.max_speed_hz=1000000;
 #function to read ADC data from a channel
 places = 3
 
-def GetData(channel):
-    adc=spi.xfer2([1,(8+channel)<<4,0])
-    data = ((adc[1]&3) << 8)+adc[2]
-    return data
-
-def ConvertVolts(data,places):
-    volts = (data*3.3)/float(1023)
-    volts=round(volts,places)
-    return volts
-
-channel = 1
-delay = .5
 
 try:
-    while True:
-        sensr_data = GetData(channel)
-        sensr_volt = ConvertVolts(sensr_data,2)
-        print(sensr_volt)
-        time.sleep(delay)
-        
+    while True:        
 except KeyboardInterrupt:
     spi.close()
